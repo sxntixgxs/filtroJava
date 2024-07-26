d40a75c47787f54909a6a4da12633438fc9d2092
sandoval <sandovalbgasantiago@gmail.com>
2024-07-27 01:33:04 +0200

    configuracion final db

--- filtro/src/main/java/com/filtro/config/DBConnection.java
+++ filtro/src/main/java/com/filtro/config/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/sgpzf";
-    private static final String USER = "campus2023";
-    private static final String PASSWORD = "campus2023";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
